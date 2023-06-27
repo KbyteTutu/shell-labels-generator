dd50c0cd10a4466ff47729db6344eaa2886126af
KbyteTutu <tukechao@gmail.com>
2023-06-27 22:11:59 +0800

    init

---
+++ shell_label_gen.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @ 南无阿弥陀佛，不要有太多bug……
+# @ Author: tukechao
+# @ Date: 2023-06-27 22:05:45
+# @ LastEditors: tukechao
+# @ LastEditTime: 2023-06-27 22:11:20
+# @ FilePath: /shell-labels-generator/shell_label_gen.py
+# @ description: Main function
+
+
+class ShellLabelGenerator(object):
+    def __init__(self):
+        pass
+
+    def input_label_framework(self):
+        d = {"name": {"value": "太阳缀壳螺", "p1": (5, 5), "p2": (20, 20), "font": "aaa"}}
+        return d
+
+    def input_excel_data(self):
+        pass
+
+    def generate_picture(self):
+        pass
